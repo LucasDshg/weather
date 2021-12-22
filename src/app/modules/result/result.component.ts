3ca4d45311c98fe08ccf9b24de4f39776a063306
LucasDshg <lucas_dshgomes@hotmail.com>
2021-12-21 23:39:05 -0300

    UI Resultado

--- src/app/modules/result/result.component.ts
+++ src/app/modules/result/result.component.ts
@@ -4,13 +4,23 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   selector: 'app-result',
   templateUrl: './result.component.html',
   styleUrls: ['./result.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResultComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getTimeText() {
+    const time = new Date().getHours();
+
+    if (time <= 12) {
+      return 'Good Morning';
+    } else if (time > 12 && time <= 18) {
+      return 'Good Afternoon';
+    } else {
+      return 'Good Night';
+    }
   }
-
 }
