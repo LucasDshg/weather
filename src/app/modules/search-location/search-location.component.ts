5810a27a9819d6a6d5fcf8ca1dc8bfe5f1fa3c63
LucasDshg <lucas_dshgomes@hotmail.com>
2022-01-09 20:01:52 -0300

    using theme service

--- src/app/modules/search-location/search-location.component.ts
+++ src/app/modules/search-location/search-location.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { ThemeService } from 'src/app/shared/service/theme.service';
 
 @Component({
   selector: 'app-search-location',
   templateUrl: './search-location.component.html',
   styleUrls: ['./search-location.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SearchLocationComponent implements OnInit {
+  iconTheme: { dark: string; light: string };
 
-  constructor() { }
+  constructor(private _themeService: ThemeService) {}
 
   ngOnInit(): void {
+    this.iconTheme = this._themeService.icons;
   }
 
+  changeTheme() {
+    this.iconTheme = this._themeService.changeTheme();
+  }
 }
